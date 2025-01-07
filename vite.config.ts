abfefa6dd14b5db25677163af65557ced900ecf9
DaniLedes99 <daniela.e.ledesma99@gmail.com>
2025-01-07 04:04:31 -0300

    up

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,8 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: "dist", // Asegúrate de que los archivos se generen en la carpeta 'dist'
+  },
   base: "/",
 });
